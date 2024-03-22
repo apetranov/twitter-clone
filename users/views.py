6edf32f543ace4561f4850cbff3adc0141b5aa97
Alexander Petranov <shasho68.5@gmail.com>
2024-03-22 20:13:12 +0200

    Finish login/ register screen design

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, "users/index.html")
+
+def login(request):
+    return HttpResponse("Login")
+
+def register(request):
+    return HttpResponse("Registration page")
+
+def profile(request, username):
+    return HttpResponse(f"{username.capitalize()}")
